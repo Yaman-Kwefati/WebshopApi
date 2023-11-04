1045669dcd31d72c8d3ebff83602569e1e4a70ee
Yaman Kwefati <64845605+Yaman-Kwefati@users.noreply.github.com>
2023-11-04 19:41:33 +0100

    Initial commit

---
+++ src/test/java/com/mealmatch/mealmatchapi/MealMatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealmatch.mealmatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMatchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
