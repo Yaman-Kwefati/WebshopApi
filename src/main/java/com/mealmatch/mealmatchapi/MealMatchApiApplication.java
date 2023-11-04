6d75b5aa3cc49afd5d4268ab2007533dc10b9575
Yaman Kwefati <64845605+Yaman-Kwefati@users.noreply.github.com>
2023-11-04 19:41:33 +0100

    Initial commit

---
+++ src/main/java/com/mealmatch/mealmatchapi/MealMatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.mealmatch.mealmatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMatchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealMatchApiApplication.class, args);
+    }
+
+}
