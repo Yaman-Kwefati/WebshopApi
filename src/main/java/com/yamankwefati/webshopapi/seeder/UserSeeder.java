742bd6ae17d107c52d01192fba3eddc906af4973
Yaman Kwefati <64845605+Yaman-Kwefati@users.noreply.github.com>
2024-01-12 18:31:47 +0100

    Feat: Added product qty get less when order is placed.

--- src/main/java/com/yamankwefati/webshopapi/seeder/UserSeeder.java
+++ src/main/java/com/yamankwefati/webshopapi/seeder/UserSeeder.java
@@ -15,26 +15,26 @@ public class UserSeeder {
     private UserDAO userDAO;
     private final PasswordEncoder passwordEncoder;
     public void seed(){
-        for (int i = 0; i < 20; i++) {
-            User newUser = User.builder()
-                    .firstname("User" + i + "firstname")
-                    .lastname("User" + i + "lastname")
-                    .email("test"+i+"@gmail.com")
-                    .password(passwordEncoder.encode("testPassword"))
-                    .phoneNumber("0625112776")
-                    .city("Bodegraven")
-                    .street("Koninginneweg 124")
-                    .postalCode("2411XV")
-                    .build();
-
-
-            this.userDAO.saveNewUser(newUser);
-        }
+//        for (int i = 0; i < 20; i++) {
+//            User newUser = User.builder()
+//                    .firstname("User" + i + "firstname")
+//                    .lastname("User" + i + "lastname")
+//                    .email("test"+i+"@gmail.com")
+//                    .password(passwordEncoder.encode("testPassword"))
+//                    .phoneNumber("0625112776")
+//                    .city("Bodegraven")
+//                    .street("Koninginneweg 124")
+//                    .postalCode("2411XV")
+//                    .build();
+//
+//
+//            this.userDAO.saveNewUser(newUser);
+//        }
         User newUser = User.builder()
-                .firstname("YamanAdmin")
-                .lastname("KwefatiAdmin")
+                .firstname("Yaman")
+                .lastname("Kwefati")
                 .email("yamankwefati@icloud.com")
-                .password(passwordEncoder.encode("testPassword"))
+                .password(passwordEncoder.encode("yamank77"))
                 .phoneNumber("0625112776")
                 .city("Bodegraven")
                 .street("Koninginneweg 124")
